fbed24055d1328dd7a70ab8748140c0ba6696511
Jinhyuck Kim <jinhyuck@Jinhyucks-MacBook-Pro-16.local>
2020-05-27 14:17:42 +0900

    start vue router

---
+++ VUE/03_first-vue-cli/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <hr>
+    <!-- step3. 사용한다. -->
+    <MyComponent />
+    <!-- <GreateComponent /> -->
+  </div>
+</template>
+
+<script>
+// 컴포넌트 사용하기
+// step1. import 한다
+import MyComponent from './components/MyComponent.vue'
+// import GreateComponent from './components/GreateComponent.vue'
+export default {
+  name: 'App',
+  components: {
+    // step2. 등록한다.
+    MyComponent,
+    // GreateComponent,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
